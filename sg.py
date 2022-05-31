f0d56ba96240d6f85c6a3f94775ff364709e529e
adriel87png <jmrabe@up.edu.ph>
2022-05-31 16:53:27 +0800

    Modified sg.py

--- sg.py
+++ sg.py
@@ -2,9 +2,151 @@ import PySimpleGUI as sg
 import io, os
 from PIL import Image
 import cv2
+import pytesseract as tes
 
 #for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+#functions for OCR
+def performOCR(img=None, grayscale=False):
+	"""
+		Description:
+			performORC() is the function that performs the OCR on the given image
+
+		Args:
+				img (cv2.imread(), optional): image to perform OCR on. Defaults to None.
+				grayscale (bool, optional): whether to pre-process the image to grayscale or not. Defaults to False.
+
+		Returns:
+				list: a list of tuples containing information of all found texts in the image
+
+	"""
+
+	# return if there is no image
+	if img is None:
+		return
+	
+	# convert image if grayscale is enabled
+	if grayscale is True:
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	# tesseract config 
+	# --oem (tesseract scanning engine)
+	# --psm (image segmentation method)
+	# -c tessedit_char_blacklist= (characters not to scan)
+	tes_config = r'--oem 3 --psm 12 -c tessedit_char_blacklist=!\"#$%&\'()*+,-./0123456789:;<=>?@[\\]^_`{|}~ '
+	raw_result = tes.image_to_data(img, config=tes_config, output_type=tes.Output.DICT)
+
+	# value to be returned
+	result = list()
+
+	# evaluate 
+	for i in range(0, len(raw_result["text"])):
+		
+		text = raw_result["text"][i] # get text
+		conf = int(raw_result["conf"][i]) # get confidence
+
+		# assume confidence threshold to 60
+		if conf > 60:
+			# if confidence level > 60
+			# safe to say that it is a positive result 
+			to_add = (text, conf)
+			result.append(to_add)
+			
+	return result
+
+
+def compareResult(coloredScan=None, grayScan=None):
+	"""
+	compareResult() is the function that compares the result of the colored scan from the grayscale scan
+
+	Args:
+			coloredScan (dict, optional): dictionary result from the colored scan of the image. Defaults to None.
+			grayScan (dict, optional): dictionary result from the grayscaled scan of the image. Defaults to None.
+
+	Returns:
+			string: the concatenated string from the result with higher confidence level
+	"""
+
+	# total confidence level, to see which result is more accurate
+	coloredTotal = 0
+	grayTotal = 0
+	coloredString = ""
+	grayString = ""
+
+	if coloredScan is not None:
+		for text,conf in coloredScan:
+			coloredTotal += int(conf)
+			coloredString = " ".join([coloredString, text])
+
+	if grayScan is not None:
+		for text,conf in grayScan:
+			grayTotal += int(conf)
+			grayString = " ".join([grayString, text])
+
+	return coloredString if coloredTotal>=grayTotal else grayString
+	
+
+
+	# return None
+
+
+def sampleUsage(img):
+	"""this is a sample usage of the two function above
+
+	Args:
+			img (cv2.imread()): image result of the cv2.imread()
+	"""
+
+	if img is None:
+		return
+
+    # run both colored and grayscaled to check
+    # if which image will return more accurate result
+	colored = performOCR(img, grayscale=False)
+	grayscaled = performOCR(img, grayscale=True)
+	return compareResult(colored, grayscaled)
+
+	# print("colored: {}".format(colored))
+	# print("grayscaled: {}".format(grayscaled))
+	# print("Result: {}".format(string_result))
+
+
+
+def generateID(face, bg, text="Name Surname"):
+    bg_width = 600
+    bg_height = 900
+    fg_height = 300
+    fg_width = 300
+
+    textBottomY = 635
+    alpha = 1
+
+    # coordinates of id picture
+    start_y = 225
+    start_x = 150
+    end_y = 525
+    end_x = 450
+
+    font = cv2.FONT_HERSHEY_DUPLEX
+    text = text.upper()
+
+    face_portion = cv2.addWeighted(face, 
+                    alpha, 
+                    bg[start_y:end_y, start_x:end_x,:], 
+                    1-alpha, 
+                    0, bg)
+
+    bg[start_y:end_y, start_x:end_x,:] = face_portion
+
+    # center the text in bg image
+    textsize = cv2.getTextSize(text, font, 1.5, 2)[0]
+    textX = int((600 -  textsize[0])/2)
+    # put the text
+    cv2.putText(bg, text, (textX, textBottomY), font, 1.5, (85, 85, 1), 2)
+
+    return bg
+
 
 #For files
 file_type = [    
@@ -18,8 +160,9 @@ file_type = [
 
 column_output = [
     
-    [sg.Text("Resulting Image")],
-    [sg.Image(key="-OUTPUTIMAGE-")]
+    [sg.Text("Converted Image Here")],
+    [sg.Image(key="-OUTPUTIMAGE-")],
+    [sg.Button("Save Image")]
     
 ]
 
@@ -104,23 +247,46 @@ while True:
             #output is this for face detection
             face_result = cv2.resize(face_result, new_size, fx=0.25, fy=0.25, interpolation=cv2.INTER_LINEAR)
 
-            # window["-OUTPUTIMAGE-"].update(data=face_result)
+            face_result = cv2.cvtColor(face_result, cv2.COLOR_BGR2RGB)
+
+
+            #-------------------------#
+            #     OCR RIGHT HERE      #
+            #-------------------------#
+
+            img = cv2.imread(values["-FILE-"])
+            ocr_result = sampleUsage(img)
+
+
+            #-------------------------#
+            #       ID CREATION       #
+            #-------------------------#
+            id_temp = cv2.imread('data/id.png')
+            id_temp = cv2.cvtColor(id_temp, cv2.COLOR_BGR2RGB)
+
+            face_result = cv2.resize(face_result, (300,300))
+            id_result = generateID(face_result, id_temp, ocr_result)
+
+            pil_image = Image.fromarray(id_result)
+            pil_image.thumbnail((400,400))
+
+            # Saves the image to DISPLAY on leftside
+            bio2 = io.BytesIO()
+            pil_image.save(bio2, format="PNG")
+
+            window["-OUTPUTIMAGE-"].update(data=bio2.getvalue())
 
             #WHAT TO DO HERE!!!
             #The goal is to send put face_result into the ID
             #Next is to put the OCR text in the ID
             #PUT the resulting ID in -OUTPUTIMAGE-
             #PUT a save button
+    elif event == "Save Image":
+        filename = str(ocr_result) + "_id.png"
+        output = cv2.cvtColor(id_temp, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(filename, output)
 
 
-
-        #Add else statement for improper file
-    # elif event == "Convert to ID":
-    #     filename = values["-FILE-"]
-    #     img = cv2.imread(values["-FILE-"])
-    #     cv2.imshow("something", img)
-    #     cv2.waitKey(0)
-    #     continue
 window.close()
 
 
